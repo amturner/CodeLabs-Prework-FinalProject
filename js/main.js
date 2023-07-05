7bc7c700de83a458bfb989547307dce708eb0cbd
Alexander Turner <cloud-alex@hotmail.co.uk>
2023-07-05 14:53:11 -0500

    Updated index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+// Form Inputs
 const adj1 = document.querySelector("#adj1");
-const adj2 = document.querySelector("#adj2");
 const noun = document.querySelector("#noun");
+const adj2 = document.querySelector("#adj2");
 const clothing = document.querySelector("#clothing");
 const bodyPart = document.querySelector("#bodyPart");
 const verb = document.querySelector("#verb");
 const food = document.querySelector("#food");
+
+// Display Mad Lib
+const madLibDisplay = document.querySelector(".mad-lib");
+
+// Form
+const form = document.querySelector("form");
+
+// Variable to contain the mad lib story
+const story = `"My friends and I started a secret club! We meet every day by the <b></b>`;
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
