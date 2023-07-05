733be49e0ea63a07a62075d4743e01174ed4b485
Alexander Turner <cloud-alex@hotmail.co.uk>
2023-07-05 17:41:54 -0500

    Updated comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// Form Inputs
+// Variables
+//Form Inputs
 const adj1 = document.querySelector("#adj1");
 const noun = document.querySelector("#noun");
 const adj2 = document.querySelector("#adj2");
@@ -7,15 +8,19 @@ const bodyPart = document.querySelector("#bodyPart");
 const verb = document.querySelector("#verb");
 const food = document.querySelector("#food");
 
-// Display Mad Lib
+//Display Mad Lib
 const madLibDisplay = document.querySelector(".madLibsForm__p");
 
-// Form
+//Form
 const form = document.querySelector("form");
 
-// Variable to contain the mad lib story
+//Variable to contain the mad lib story
 const story = `My friends and I started a secret club! We meet every day by the <b>${adj1} ${noun}</b>. Our club has some very special rules. If you want to join, you have to wear a <b>${adj2} ${clothing}</b> on your <b>${bodyPart}</b>. You are not allowed to <b>${verb}</b> during meetings, and you can only eat <b>${food}</b> for your meals. If you are interested in joining, let me know!`;
 
+
+
+// Event Listeners
+//Code for handling "submit" events upon form completion
 form.addEventListener("submit", (event) => {
     // Prevent default form behaviour
     event.preventDefault();
@@ -32,13 +37,17 @@ form.addEventListener("submit", (event) => {
     : displayMadLibStory();
 });
 
+//Code for handling "reset" events upon reset button being pressed
 form.addEventListener("reset", () => {
     // Clear mad lib story from paragraph
     //Text inputs automatically cleared by the "reset" action
     clearMadLibStory();
 });
 
-// Function for displaying the mad lib story
+
+
+// Functions
+//Function for displaying the mad lib story
 displayMadLibStory = () => {
     madLibDisplay.innerHTML = story
     .replace(`${adj1}`, adj1.value)
@@ -50,7 +59,7 @@ displayMadLibStory = () => {
     .replace(`${food}`, food.value);
 };
 
-// Function for clearing the mad lib story
+//Function for clearing the mad lib story
 clearMadLibStory = () => {
     madLibDisplay.innerHTML = "";
 };
