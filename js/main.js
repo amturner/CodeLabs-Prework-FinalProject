fb0f9b7681452cb7d95e25cafbe630feabb36517
Alexander Turner <cloud-alex@hotmail.co.uk>
2023-07-05 14:47:59 -0500

    Added variables form inputs

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const adj1 = document.querySelector("#adj1");
+const adj2 = document.querySelector("#adj2");
+const noun = document.querySelector("#noun");
+const clothing = document.querySelector("#clothing");
+const bodyPart = document.querySelector("#bodyPart");
+const verb = document.querySelector("#verb");
+const food = document.querySelector("#food");
